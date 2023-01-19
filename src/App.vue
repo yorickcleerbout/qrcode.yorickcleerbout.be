37a71bc7eb56e5fc764c4a6f45f055680d5c781f
Yorick Cleerbout <yorick.cleerbout@i8c.be>
2023-01-19 18:31:03 +0100

    Added footer component to the app

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,21 @@
     <HeroText :title="'QR Code Generator'" :subTitle="'Insert a link or text below to generate a QR Code'"/>
     <LogicComponents :placeholder="'Insert link or text here...'" />
   </div>
+  <FooterComponent/>
   
 </template>
 
 <script>
 import HeroText from './components/HeroText.vue'
 import LogicComponents from './components/LogicComponents.vue'
+import FooterComponent from './components/FooterComponent.vue'
 
 export default {
   name: 'App',
   components: {
     HeroText,
-    LogicComponents
+    LogicComponents,
+    FooterComponent
   }
 }
 </script>
@@ -28,6 +31,8 @@ export default {
 }
 
 .container {
-  margin-top: 6%;
+  margin-top: 5%;
+  min-height: 62.1vh;
 }
+
 </style>
